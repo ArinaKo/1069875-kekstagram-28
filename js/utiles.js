2bce1cf5a46a401b964ab517e98728417baee304
ArinaKo <arinakovalenco7@gmail.com>
2023-03-04 16:34:24 +0300

    Разделяет код на модули

---
+++ js/utiles.js
@@ -0,0 +1,18 @@
+const getRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max + 1 - min) + min);
+
+function generateId(min, max) {
+  const usedIds = [];
+  return function () {
+    let newId = getRandomNumber(min, max);
+    while (usedIds.includes(newId)) {
+      newId = getRandomNumber(min, max);
+    }
+    usedIds.push(newId);
+    return newId;
+  };
+}
+
+const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
+
+export { getRandomNumber, generateId, getRandomElement };
