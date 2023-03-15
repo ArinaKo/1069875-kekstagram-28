a12c0cfd17325aca388709d3f68a8313c3e8fb78
ArinaKo <arinakovalenco7@gmail.com>
2023-03-15 21:49:51 +0300

    Отрисовывает сгенерированные миниатюры

---
+++ js/thumbnails.js
@@ -0,0 +1,20 @@
+import { generatePosts } from './data.js';
+
+const pictures = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const thumbnailsDate = generatePosts();
+
+const thumbnailsFragment = document.createDocumentFragment();
+
+thumbnailsDate.forEach(({url, likes, comments}) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+  thumbnailsFragment.append(thumbnail);
+});
+
+pictures.append(thumbnailsFragment);
