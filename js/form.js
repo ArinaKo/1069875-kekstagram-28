96979d863d443504aaf8fdbf62c9461627d9e99a
ArinaKo <arinakovalenco7@gmail.com>
2023-03-23 22:30:14 +0300

    Добавляет функционал для формы: очистка и события

--- js/form.js
+++ js/form.js
@@ -1,14 +1,33 @@
 import { openPopup, onCloseButtonClick } from './popup.js';
+import { isKeyEscape } from './utilities.js';
+import './form-validation.js';
 
 const form = document.querySelector('.img-upload__form');
 const fileField = form.querySelector('#upload-file');
 const formOverlay = form.querySelector('.img-upload__overlay');
 const closeButton = form.querySelector('#upload-cancel');
+const hashtagsInput = form.querySelector('.text__hashtags');
+const descriptionInput = form.querySelector('.text__description');
 
-const clearForm = () => {
-  fileField.value = '';
+const onInputEscapeKeydown = (evt) => {
+  if (isKeyEscape(evt)) {
+    evt.stopPropagation();
+  }
 };
 
-fileField.addEventListener('change', () => openPopup(formOverlay, clearForm));
+const openForm = () => {
+  hashtagsInput.addEventListener('keydown', onInputEscapeKeydown);
+  descriptionInput.addEventListener('keydown', onInputEscapeKeydown);
+};
+
+const closeForm = () => {
+  form.reset();
+  hashtagsInput.removeEventListener('keydown', onInputEscapeKeydown);
+  descriptionInput.removeEventListener('keydown', onInputEscapeKeydown);
+};
+
+fileField.addEventListener('change', () => openPopup(formOverlay, openForm, closeForm));
 
 closeButton.addEventListener('click', onCloseButtonClick);
+
+
